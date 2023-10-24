aaa4d401501992a9f86569b7b240bcc772e835ae
Sam Schlinkert <sschlinkert@gmail.com>
2023-10-24 14:34:21 -0400

    increases sample size and significance level of benchmark

--- benches/generate_passphrase.rs
+++ benches/generate_passphrase.rs
@@ -2,12 +2,17 @@ use criterion::{criterion_group, criterion_main, Criterion};
 use phraze::*;
 
 fn criterion_benchmark(c: &mut Criterion) {
+    // Define a Criterion group, just so we can set a sample_size
+    let mut group = c.benchmark_group("Generate a passphrase");
+    group.sample_size(1200).significance_level(0.1);
+
     let number_of_words = None;
     let minimum_entropy = Some(80);
     let separator = "-";
     let title_case = false;
     let list_choice = List::Medium;
-    c.bench_function("as is", |b| {
+
+    group.bench_function("as is", |b| {
         b.iter(|| {
             generate_passphrase(
                 number_of_words,
