334de6f368b46f436f5050a7e67e132bfd7728e5
Sam Schlinkert <sschlinkert@gmail.com>
2023-10-24 19:25:04 -0400

    use write macro throughout the words functions, where possible

--- build.rs
+++ build.rs
@@ -5,20 +5,21 @@ use std::path::Path;
 
 // https://doc.rust-lang.org/cargo/reference/build-scripts.html#case-study-code-generation
 
+/// Write the words from the word list file into a Rust Array for program's use.
 fn words(mut f_dest: &File, const_name: &str, fname_src: &str, list_size: usize) {
-    // Declare a new Rust constant
-    // make it an array of slices of the exact size of this word list
-    // to maximize effiency.
+    // Declare a new Rust constant that is an array of slices.
+    // To maximize efficiency, make it the exact size of this word list.
     write!(f_dest, "const {const_name}: &[&str; {list_size}] = &[").unwrap();
 
     // Read words in and add them to this array
     let f_src = BufReader::new(File::open(fname_src).unwrap());
-    for line in f_src.lines() {
-        f_dest.write_all(b"\"").unwrap();
-
-        f_dest.write_all(line.unwrap().as_bytes()).unwrap();
-
-        f_dest.write_all(b"\",").unwrap();
+    for word in f_src.lines() {
+        match word {
+            // We're writing a Rust Array programmtically, so need the word to be surround by
+            // double quotes and have a comma between words.
+            Ok(word) => write!(f_dest, "\"{word}\",").unwrap(),
+            Err(_e) => panic!("Error reading line from built-in list"),
+        }
     }
 
     // Close array syntax
