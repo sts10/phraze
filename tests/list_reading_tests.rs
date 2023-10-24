6d3ec5960e68f2ff65b3c3d23aff98565c0c4654
Sam Schlinkert <sschlinkert@gmail.com>
2023-10-24 11:51:50 -0400

    adds a test for reading in proper length

--- tests/list_reading_tests.rs
+++ tests/list_reading_tests.rs
@@ -2,6 +2,27 @@ mod minimum_entropy_tests {
     use phraze::*;
 
     #[test]
+    fn can_read_in_lists_and_find_appropriate_number_of_words() {
+        let list = make_list(List::Long);
+        assert!(list.len() == 17576);
+
+        let list = make_list(List::Qwerty);
+        assert!(list.len() == 1296);
+
+        let list = make_list(List::Alpha);
+        assert!(list.len() == 1296);
+
+        let list = make_list(List::Eff);
+        assert!(list.len() == 7776);
+
+        let list = make_list(List::Effshort);
+        assert!(list.len() == 1296);
+
+        let list = make_list(List::Mnemonicode);
+        assert!(list.len() == 1633);
+    }
+
+    #[test]
     fn can_read_in_lists_without_any_blank_words() {
         let list = make_list(List::Medium);
         assert!(!list.contains(&""));
